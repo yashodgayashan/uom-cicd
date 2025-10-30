238c3dbd9b10baee4385776cf0f0fbb4f18ed3b6
yashodgayashan <32588172+yashodgayashan@users.noreply.github.com>
2025-10-30 17:10:31 +0530

    update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, GitHub Actions with Docker!');
+    res.send('Hello, GitHub Actions with Docker!   ');
 });
 
 app.listen(PORT, () => {
